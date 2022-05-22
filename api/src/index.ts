31e4c3b80f9b7981e1e529edab67fd5ae2934f0d
Andrew Durnford <andrewdurnford3@gmail.com>
2022-05-22 14:34:49 +1000

    Initial commit

---
+++ api/src/index.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { ApolloServer } from "apollo-server-express";
+import * as cors from "cors";
+import * as express from "express";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+const prisma = new PrismaClient();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => ({ prisma }),
+});
+
+server.start().then(() => server.applyMiddleware({ app }));
+
+app.listen({ port: 4000 }, () => {
+  console.log(`🚀  Server ready at http://localhost:4000${server.graphqlPath}`);
+});
