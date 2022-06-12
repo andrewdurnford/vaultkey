862d0b0d97800f2a75f0f1026187ac3e82d4189f
Andrew Durnford <andrewdurnford3@gmail.com>
2022-06-12 13:32:21 +1000

    refactor: Item vault styling

--- react/src/components/Items.tsx
+++ react/src/components/Items.tsx
@@ -1,9 +1,28 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import useAuth from "../hooks/useAuth";
 import { useItemsQuery } from "../types/graphql";
 import { decrypt } from "../utils/encryption";
 import { ItemCreate } from "./ItemCreate";
 import { Main } from "./Main";
+
+const Stack = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Card = styled.div`
+  list-style-type: none;
+  padding: 1rem;
+  border: 1px solid black;
+  border-radius: 6px;
+`;
 
 export function Items() {
   const navigate = useNavigate();
@@ -21,38 +40,31 @@ export function Items() {
 
   return (
     <Main>
-      <h1>Items</h1>
-      {data?.items.length === 0 ? (
-        <div>No items</div>
-      ) : (
-        <ul>
-          {data?.items.map((item) => {
-            const decrypted = !!item.password
-              ? decrypt(item.password, secretKey!)
-              : null;
-            return (
-              <li key={item.id}>
-                <h4>{item.name}</h4>
-                <ul>
-                  <li>
-                    <b>Username: </b>
-                    {item.username}
-                  </li>
-                  <li>
-                    <b>Password: </b>
-                    {item.password}
-                  </li>
-                  <li>
-                    <b>Decrypted: </b>
-                    {decrypted}
-                  </li>
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      <ItemCreate />
+      <Stack>
+        <h1>Items</h1>
+        {data?.items.length === 0 ? (
+          <div>No items</div>
+        ) : (
+          <Stack as="ul">
+            {data?.items.map((item) => {
+              const decrypted = !!item.password
+                ? decrypt(item.password, secretKey!)
+                : null;
+              return (
+                <Card as="li" key={item.id}>
+                  <Container>
+                    <h3>{item.name}</h3>
+                    <span>{item.username}</span>
+                    <span>{item.password}</span>
+                    <span>{decrypted}</span>
+                  </Container>
+                </Card>
+              );
+            })}
+          </Stack>
+        )}
+        <ItemCreate />
+      </Stack>
     </Main>
   );
 }
--- react/src/components/Main.tsx
+++ react/src/components/Main.tsx
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Main = styled.main`
   margin: 0 auto;
   padding: 2rem;
-  max-width: 75rem;
+  max-width: 48rem;
 `
